c84aa721f9ddb715051f9d222590f8fe99e6aaac
Matthias <matthias.karl@cmdscale.com>
2022-12-04 20:32:26 +0100

    feat: day 4 part 2

--- src/day_four.rs
+++ src/day_four.rs
@@ -9,10 +9,16 @@ fn section_in_range(range: Range<u32>, section: Vec<u32>) -> bool {
     }
     false
 }
+fn section_overlapping(range: Range<u32>, section: Vec<u32>) -> bool {
+    if range.contains(&section[0]) || range.contains(&section[1]) {
+        return true;
+    }
+    false
+}
 pub async fn day_four() -> Result<(), Error> {
     let res = read_input("https://adventofcode.com/2022/day/4/input").await?;
     // let zones: Vec<u8> = "5-13".split("-").map(|i| i.parse::<u8>()).collect();
-    let contains: u32 = res
+    let parsed_assignments: Vec<Vec<Vec<u32>>> = res
         .split('\n')
         .map(|section_assignment| {
             let sections: Vec<Vec<u32>> = section_assignment
@@ -30,6 +36,13 @@ pub async fn day_four() -> Result<(), Error> {
                         .collect()
                 })
                 .collect();
+            sections
+        })
+        .collect();
+
+    let part_one: u32 = parsed_assignments
+        .iter()
+        .map(|sections| {
             let mut sum_overlapping: u32 = 0;
             if sections.len() > 1 {
                 let first_section = sections[0].clone();
@@ -51,7 +64,55 @@ pub async fn day_four() -> Result<(), Error> {
             sum_overlapping
         })
         .sum();
-
-    println!("Day 4: {}", contains);
+    let part_one: u32 = parsed_assignments
+        .iter()
+        .map(|sections| {
+            let mut sum_overlapping: u32 = 0;
+            if sections.len() > 1 {
+                let first_section = sections[0].clone();
+                let second_section = sections[1].clone();
+                let range1 = Range {
+                    start: first_section[0],
+                    end: first_section[1] + 1,
+                };
+                let range2 = Range {
+                    start: second_section[0],
+                    end: second_section[1] + 1,
+                };
+                if section_in_range(range1, second_section)
+                    || section_in_range(range2, first_section)
+                {
+                    sum_overlapping += 1;
+                }
+            }
+            sum_overlapping
+        })
+        .sum();
+    let part_two: u32 = parsed_assignments
+        .iter()
+        .map(|sections| {
+            let mut sum_overlapping: u32 = 0;
+            if sections.len() > 1 {
+                let first_section = sections[0].clone();
+                let second_section = sections[1].clone();
+                let range1 = Range {
+                    start: first_section[0],
+                    end: first_section[1] + 1,
+                };
+                let range2 = Range {
+                    start: second_section[0],
+                    end: second_section[1] + 1,
+                };
+                if section_overlapping(range1, second_section)
+                    || section_overlapping(range2, first_section)
+                {
+                    sum_overlapping += 1;
+                }
+            }
+            sum_overlapping
+        })
+        .sum();
+    println!("Day 4: {}", part_one);
+    println!("Day 4: {}", part_two);
     Ok(())
 }
