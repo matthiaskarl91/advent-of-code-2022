6b45bb80dfa4ca70f08f6a1734fa7b1f0b11d43c
Matthias <matthias.karl@cmdscale.com>
2022-12-11 18:58:51 +0100

    day 8

--- src/day_eight.rs
+++ src/day_eight.rs
@@ -8,11 +8,11 @@ use crate::read_input::read_input;
 struct Tree {
     x: usize,
     y: usize,
-    height: usize,
+    height: i8,
 }
 
 impl Tree {
-    fn new(x: usize, y: usize, height: usize) -> Self {
+    fn new(x: usize, y: usize, height: i8) -> Self {
         Self { x, y, height }
     }
 }
@@ -29,13 +29,14 @@ fn visible(forest: &Forest, tree: &Tree) -> bool {
         .iter()
         // .filter(|t| t.x != tree.x && t.y != tree.y)
         .any(|t| {
-            println!("in Forest: {:?}, tree: {:?}", t, tree);
+            // println!("in Forest: {:?}, tree: {:?}", t, tree);
             if t.x == tree.x && t.y == tree.y && t.height == tree.height {
                 return false;
             } else if t.x == tree.x && t.height < tree.height {
                 // same row
                 // if t.height < tree.height {
                 // is_visible = true;
+
                 return true;
                 // }
             } else if t.y == tree.y && t.height < tree.height {
@@ -47,43 +48,44 @@ fn visible(forest: &Forest, tree: &Tree) -> bool {
                 return false;
             }
         });
-    println!("{}", is_visible);
+    // println!("{}", is_visible);
     return is_visible;
 }
 
-fn is_visible(forest: &Vec<Vec<usize>>, tree: &Tree) -> bool {
-    let mut row_idx = 0;
-    for row in forest {
-        let mut col_idx = 0;
-        for col in row {
-            if tree.y == row_idx {
-                if col < &tree.height {
-                    println!(
-                        "x: {}, y: {}, height: {} vs: {:?}",
-                        row_idx, col_idx, col, tree
-                    );
-                    return true;
-                    // } else {
-                    //     print
-                }
-            }
-            col_idx += 1;
-        }
-        row_idx += 1;
-    }
+// fn is_visible(forest: &Vec<Vec<usize>>, tree: &Tree) -> bool {
+//     let mut row_idx = 0;
+//     for row in forest {
+//         let mut col_idx = 0;
+//         for col in row {
+//             if tree.y == row_idx {
+//                 if col < &tree.height {
+//                     println!(
+//                         "x: {}, y: {}, height: {} vs: {:?}",
+//                         row_idx, col_idx, col, tree
+//                     );
+//                     return true;
+//                     // } else {
+//                     //     print
+//                 }
+//             }
+//             col_idx += 1;
+//         }
+//         row_idx += 1;
+//     }
 
-    return true;
-}
+//     return true;
+// }
 
 pub async fn execute() -> Result<(), Error> {
     // let res = read_input("https://adventofcode.com/2022/day/8/input").await?;
     let mut res = fs::read_to_string("day_eight.txt").unwrap();
     res.truncate(res.len() - 1);
-    let forest_vec: Vec<Vec<usize>> = res
+    let forest_vec: Vec<Vec<i8>> = res
         .split("\n")
         .map(|row| {
             row.split("")
-                .map(|tree| Some(tree.parse::<usize>()))
+                .map(|tree| tree.parse::<i8>().unwrap_or(-1))
+                .filter(|t| t >= &0)
                 .collect()
         })
         .collect();
@@ -103,6 +105,16 @@ pub async fn execute() -> Result<(), Error> {
     }
 
     forest.data.iter().for_each(|tree| {
+        if tree.x == 0 || tree.x == forest_vec.len() - 1 {
+            // result += 0;
+            // continue;
+            return;
+        }
+        if tree.y == 0 || tree.y == forest_vec[0].len() - 1 {
+            // result += 0;
+            return;
+        }
+        println!("{:?}", tree);
         if visible(&forest, tree) {
             result += 1;
         }
